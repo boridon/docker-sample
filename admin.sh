d4d05e8321e301799e171dbd3f2731216da1c108
boridon <boridon@hotmail.co.jp>
2017-12-21 18:25:20 +0900

    - sample_adminコンテナがdocker-compose up でつくられてしまうので、   admin.sh でsample_adminコンテナがあれば強制削除するようにした - phalcon migration generate でまずテーブルをつくれ、というわれたのでつくってみた

--- admin.sh
+++ admin.sh
@@ -1,2 +1,6 @@
 #! /bin/sh
+CONT=`docker ps -aq --filter "name=sample_admin"`
+if [ -n "$CONT" ]; then
+  docker rm --force sample_admin
+fi
 docker run --rm --name sample_admin --net=dockersample_default -v $(pwd)/project:/home/docker/project:rw -i -t sample/admin /bin/bash --login
