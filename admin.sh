9ff1b642369b391439291243438b2ecbbb4a7178
boridon <boridon@hotmail.co.jp>
2017-12-25 18:20:35 +0900

    sample_adminコンテナをcron実行環境に変更した

--- admin.sh
+++ admin.sh
@@ -1,6 +1,11 @@
 #! /bin/sh
-CONT=`docker ps -aq --filter "name=sample_admin"`
-if [ -n "$CONT" ]; then
-  docker rm --force sample_admin
-fi
-docker run --rm --name sample_admin --net=dockersample_default -v $(pwd)/project:/home/docker/project:rw -i -t sample/admin /bin/bash --login
+
+# 毎回コンテナを作り直す場合
+#CONT=`docker ps -aq --filter "name=sample_admin"`
+#if [ -n "$CONT" ]; then
+#  docker rm --force sample_admin
+#fi
+#docker run --rm --name sample_admin --net=dockersample_default -e "APP_ENV=local" -v $(pwd)/project:/home/docker/project:rw -i -t sample/admin /bin/bash --login
+
+# 実行中コンテナに接続
+docker exec -it sample_admin /bin/bash --login
